ec8a84247f00b550d13d41b1eb7c742f344e132d
jamesespericueta <espericueta2303@gmail.com>
2022-08-01 12:29:25 -0400

    update

--- index.js
+++ index.js
@@ -4,10 +4,42 @@ dotenv.config()
 
 //TODO: Get the beginning of the bot ready for a game
 //      Get the players that want to play as part of the game where they would be stored
+// TODO: Make objects for each player so that they have a political party state
+
+/*
+LEts think this through
+maybe we dont have to make an object because there are only two important roles
+*/
+
+class Player
+{
+    constructor(isHitler, isLiberal, isFacist)
+    {
+        this.isHitler = isHitler;
+        this.isLiberal = isLiberal;
+        this.isFascist = isFascist;
+    }
+
+    isFascist()
+    {
+        return this.isFascist;
+    }
+    isLiberal()
+    {
+        return this.isLiberal;
+    }
+    isHitler()
+    {
+        return this.isHitler;
+    }
+}
+
 const prefix = '!'
+
 let gameIsOngoing = false;
 let playerArr = [];
 let isAddingPlayers = false;
+
 
 const client = new DiscordJS.Client({
     intents: [
@@ -15,23 +47,35 @@ const client = new DiscordJS.Client({
         Intents.FLAGS.GUILD_MESSAGES
     ]
 });
+
+
 const nameAllPlayers = () =>
 {
-    console.log(playerArr);
-    let playerNames ='';
+    let playerNames = [];
     playerArr.forEach(user => {
-        if(playerArr.indexOf(user) == playerArr.length--)
-        {
-            playerNames += user.username;
-        }
-        else 
-        {
-            playerNames += user.username + ", ";
-        }
+        playerNames.push(user.username);
     });
-    return playerNames;
+    return playerNames.join(', ');
 }
 
+const designateRoles = () =>
+{
+    if(playerArr.length >= 5 && playerArr.length <= 6)
+    {
+
+    }
+    else if(playerArr.length >= 7 && playerArr.length <= 8)
+    {
+
+    }
+    else
+    {
+
+    }
+}
+
+
+//processing the commands
 client.on('messageCreate', (message) => {
     if(!message.content.startsWith(prefix) || message.author.bot) return;
     
@@ -54,19 +98,18 @@ client.on('messageCreate', (message) => {
     {
         if(isAddingPlayers)
         {
-            //check if theres already player limit
+            //check if theres already user limit
             if(playerArr.length <= 10)
             {
-                let player = message.author;
-                if(!playerArr.includes(player))
+                let user = message.author;
+                if(!playerArr.includes(user))
                 {
-                    playerArr.push(player);
+                    playerArr.push(user);
                 }
                 else 
                 {
                     message.reply("You are already a part of the game");
                 }
-
             }
             console.log(nameAllPlayers());
         }
@@ -84,18 +127,31 @@ client.on('messageCreate', (message) => {
             {
                 isAddingPlayers = false;
                 message.channel.send('The players participating are ' + nameAllPlayers);
+
+
             }
             else
             {
-                    message.reply("There aren't enought participants");
+                    message.reply("You only have " + playerArr.length + "players. You need between 5 and 10 players to play");
             }
         }
 
     }    
 })
 
+
+//you can retain a game state everytime a command is thrown
+/*
+    For example:
+    You can just run a sequence of code everytime that a person is given a the choice of cards
+    and everytime after they choose the next person they nominate to be chancellor
+    In the end it is a finite state machine and can be paused at any moment to wait for 
+    player input to cause the machine to change state
+*/
 client.on('ready', () => {
     console.log('The mf bot is ready');
 })
+
+
 client.login(process.env.TOKEN);
 
